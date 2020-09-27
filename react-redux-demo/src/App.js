237790f0e319dd663353fe2395d92b9ae264bbcd
amemske <olywave@gmail.com>
2020-09-27 19:01:30 +0300

    react-redux tutorial progress

---
+++ react-redux-demo/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Provider} from 'react-redux';
+import store from './redux/store'
+import './App.css';
+import CakeContainer from './components/CakeContainer';
+import HooksCakeContainer from './components/HooksCakeContainer';
+import IceCreamContainer from './components/IceCreamContainer';
+import NewCakeContainer from './components/NewCakeContainer';
+import ItemContainer from './components/ItemContainer';
+
+function App() {
+  return (
+    <Provider store={store}>
+    <div className="App">
+      <ItemContainer/>
+      <ItemContainer cake />
+      <HooksCakeContainer/>
+      <CakeContainer />
+      <IceCreamContainer />
+      <NewCakeContainer/>
+    </div>
+    </Provider>
+  );
+}
+
+export default App;
